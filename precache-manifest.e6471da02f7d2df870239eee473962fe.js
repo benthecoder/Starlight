e1cbba3f5b694d85e1688aaadd86b3553dd68fa8
Benthecoder <benthecoder07@gmail.com>
2020-04-12 16:26:57 +0800

    Updates

---
+++ precache-manifest.e6471da02f7d2df870239eee473962fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06c10704a6dad5d771c492bb056baaac",
+    "url": "/starlight-react-app/index.html"
+  },
+  {
+    "revision": "2b6d611f0a7d4fe854ad",
+    "url": "/starlight-react-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "463c2cb0a42e149790ba",
+    "url": "/starlight-react-app/static/js/2.d34efd8f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/starlight-react-app/static/js/2.d34efd8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b6d611f0a7d4fe854ad",
+    "url": "/starlight-react-app/static/js/main.f8c86de3.chunk.js"
+  },
+  {
+    "revision": "138b5acd6a66f2d99e89",
+    "url": "/starlight-react-app/static/js/runtime-main.104940db.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/starlight-react-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
